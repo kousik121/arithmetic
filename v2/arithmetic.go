ac1ac8435e5f1940ffd67da705294168b0299df5
kousik121 <turaga128@gmail.com>
2024-03-17 01:24:15 -0400

    is even function added

---
+++ v2/arithmetic.go
@@ -0,0 +1,14 @@
+package arithmetic
+
+func IsPrime(num int) bool {
+	for i := 2; i < int(num/2); i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func IsEven(num int) bool {
+	return num%2 == 0
+}
